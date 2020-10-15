2cc769162617930693e798584448da7f88f55d5b
ShaneKlokanovski <55886031+ShaneKlokanovski@users.noreply.github.com>
2020-10-14 22:48:53 -0400

    Assignment 2 first commit

--- Assignment1/src/app/app.module.ts
+++ Assignment1/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent,
+    ContentListComponent
   ],
   imports: [
     BrowserModule
