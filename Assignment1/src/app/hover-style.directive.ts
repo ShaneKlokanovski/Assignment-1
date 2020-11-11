4049d7c965df5e13a16126ef3e1dba31d3ddf17d
ShaneKlokanovski <55886031+ShaneKlokanovski@users.noreply.github.com>
2020-11-10 20:15:21 -0500

    assignment 4 fix

--- Assignment1/src/app/hover-style.directive.ts
+++ Assignment1/src/app/hover-style.directive.ts
@@ -1,18 +1,36 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHoverStyle]'
 })
 export class HoverStyleDirective {
 
+  @Input() changeTextStyle: string;
+
   constructor(private elm: ElementRef, private rend: Renderer2) {
   }
 
   @HostListener('mouseenter') onMouseEnter(): void{
-    this.rend.setStyle(this.elm.nativeElement, 'text-decoration', 'underline');
+    if (this.changeTextStyle)
+    {
+      if (this.changeTextStyle === 'textDecoration') {
+      this.elm.nativeElement.style.textDecoration = 'underline';
+    }
+    else if (this.changeTextStyle === 'fontWeight') {
+      this.elm.nativeElement.style.fontWeight = 'bold';
+    }
+    }
   }
 
   @HostListener('mouseleave') onMouseLeave(): void{
-    this.rend.setStyle(this.elm.nativeElement, 'text-decoration', null);
+    if (this.changeTextStyle)
+    {
+      if (this.changeTextStyle === 'textDecoration') {
+        this.elm.nativeElement.style.textDecoration = 'none';
+      }
+      else if (this.changeTextStyle === 'fontWeight') {
+        this.elm.nativeElement.style.fontWeight = 'initial';
+      }
+    }
   }
 }
